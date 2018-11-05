c665f4a99b679214a7f6067772194442ae16eef5
saumitra2810 <saumitra.khanwalker@swymcorp.com>
2018-11-05 08:01:54 +0530

    Added news stub

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/faqs', function(req, res, next) {
   res.render('faqs', {});
 });
 
+router.get('/news-test', function(req, res, next) {
+  res.render('news-widget', {});
+});
+
 router.get('/posters-test', function(req, res, next) {
   res.render('posters', {posters: [{ lang: 'English', link: 'http://plateletdonors.org/bcp/download.php?download_file=Poster PD English.pdf&type=p' }]});
 });
