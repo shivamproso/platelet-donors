9160186235b7612ec87efc686e08288b99069c89
harish-harry <harish.k@etag.in>
2018-11-07 22:56:06 +0530

    Functions for hashing and hashcomparing password

---
+++ utils/security.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+var BCRYPT_SALT_ROUNDS = 12;
+
+/**
+ *
+ *
+ * @param {*} str
+ * @returns {*} hash
+ */
+exports.getHashedValue = async (str) => {
+    let hash = await bcrypt.hash(str, BCRYPT_SALT_ROUNDS);
+    if(hash){
+        return hash;
+    };
+}
+/**
+ *
+ *
+ * @param {*} str
+ * @param {*} hash
+ * @returns {Boolean} res
+ */
+exports.checkHash = async (str, hash) => {
+    let res = bcrypt.compare(str, hash);
+    return res;
+}
