950c902e7516a6ff65a4bf36e3647b305a2a4815
Shratterjack <shreyanshs256@gmail.com>
2018-11-12 14:02:35 +0530

    Adding the function to add entries into the tblapilog.

--- controller.js
+++ controller.js
@@ -24,5 +24,50 @@ module.exports = {
     }).then(countries => {
       res(countries);
     });
+  },
+
+
+/**
+  * creates api log from req body by taking paramters from it
+  * @function setApi
+  * @param {String} userid The userid (primary key) to get a user.
+  * @return {json} status and an object in json format
+  */
+  setApi:function(req,res){
+    var param = req.body;
+    if(Object.keys(param).length == 0 && param.constructor === Object)
+    {
+/* This is just a dummy JSON object which will be passed as
+ * a parameter when the request body is empty
+ * It's just for naive testing(POSTMAN) and will be deleted in near future when not needed
+ */
+      param =  {
+      caller: "Shreyansh",
+      bloodgroup: "A+",
+      stdcode: "022",
+      pincode:"400706",
+      apicode:"w1hbr3br4",
+      donorfound:1,
+      donormobile:'8767903336',
+      ipaddress:'127.0.0.1',
+      remarks:"good"
+    };
+    }
+    models.apilog.create(
+      param
+   ).then(function(log){
+      res.status(201).json({
+        status:'success',
+        data:log
+      });
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        status:'error',
+        data: err
+      });
+    })
   }
+
+
 };
